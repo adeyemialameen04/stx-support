0df5fb214c870127a0f4aabbaa3f0faec93e6fca
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-04 13:34:53 +0100

    feat: globals setup

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "@repo/globals/globals";
-import "./globals.css";
-
+import "@repo/ui/globals.css";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
