6cf8bd93422d438dbc72fea2d635ab67e7d39644
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-07 19:39:18 +0100

    implemented authentication and authorization

--- apps/backend/src/posts/routes.ts
+++ apps/backend/src/posts/routes.ts
@@ -1,15 +1,17 @@
 import bearer from "@elysiajs/bearer";
 import Elysia, { t } from "elysia";
-import { selectPostSchema } from "../db/schema/post";
+import { insertPostSchema, selectPostSchema } from "../db/schema/post";
 import jwt from "@elysiajs/jwt";
 import { PayloadModel } from "../models/auth";
 import env from "../env";
-import { ERRORS } from "../models/error-models";
 import { postService } from "./service";
 import { accessTokenSecurity } from "../utils/helpers";
+import { AuthorizationError } from "../exceptions/errors";
+import { ERRORS } from "../models/error-models";
 
 const tags = ["Posts"];
-export const postsRoutes = new Elysia({ prefix: "/posts", tags })
+
+export const authService = new Elysia({ name: "auth/service" })
   .use(bearer())
   .use(
     jwt({
@@ -19,57 +21,58 @@ export const postsRoutes = new Elysia({ prefix: "/posts", tags })
       schema: PayloadModel,
     }),
   )
-  .get(
-    "",
-    async ({ bearer, accessJwt, error }) => {
-      const payload = await accessJwt.verify(bearer);
-      if (payload) {
-        const userPosts = await postService.getUserPosts(payload.user.id);
+  .guard({
+    as: "scoped",
+  })
+  .derive({ as: "scoped" }, async ({ bearer, accessJwt }) => {
+    if (!bearer) {
+      return { payload: null };
+    }
 
-        return userPosts;
-      }
+    const token = await accessJwt.verify(bearer);
 
-      return error("Unauthorized", {
-        status: 401,
-        detail: "Bearer Token Required",
-        error: "Unauthorized",
-      });
-    },
+    return {
+      payload: token,
+    };
+  });
+
+export const postsRoutes = new Elysia({ prefix: "/posts", tags })
+  .use(authService)
+  .guard(
     {
-      response: {
-        200: t.Array(selectPostSchema),
-        401: ERRORS.UNAUTHORIZED,
-      },
-      detail: {
-        security: accessTokenSecurity,
-        summary: "Get user Posts",
-        description: "Get currently logged in users posts",
-      },
-      async beforeHandle({ bearer, error, accessJwt }) {
-        if (!bearer) {
-          return error("Unauthorized", {
-            status: 401,
-            detail: "Bearer Token Required",
-            error: "Unauthorized",
-          });
-        }
-
-        const payload = await accessJwt.verify(bearer);
+      async beforeHandle({ payload, set }) {
         if (!payload) {
-          return error(401, {
-            status: 401,
-            detail: "Invalid token",
-            error: "Unauthorized",
-          });
-        }
-
-        if (payload.refresh) {
-          return error(401, {
-            status: 401,
-            detail: "Invalid token type. Access token required.",
-            error: "Unauthorized",
-          });
+          throw new AuthorizationError("Error: Hello");
         }
       },
     },
+    (app) =>
+      app
+        .get(
+          "",
+          async ({ error, payload }) => {
+            console.log(payload, "handler");
+            if (payload) {
+              const userPosts = await postService.getUserPosts(payload.user.id);
+
+              return userPosts;
+            }
+
+            throw new AuthorizationError("Bearer token required");
+          },
+          {
+            response: {
+              200: t.Array(selectPostSchema),
+              401: ERRORS.UNAUTHORIZED,
+            },
+            detail: {
+              security: accessTokenSecurity,
+              summary: "Get user Posts",
+              description: "Get currently logged in users posts",
+            },
+          },
+        )
+        .post("", async ({ body, payload }) => {}, {
+          body: insertPostSchema,
+        }),
   );
--- apps/backend/src/posts/service.ts
+++ apps/backend/src/posts/service.ts
@@ -2,10 +2,13 @@ import { eq } from "drizzle-orm";
 import { db } from "../db";
 import { postTable } from "../db/schema";
 
+// type Post =
 export const postService = {
   getUserPosts: async (id: string) => {
-    const posts = db.select().from(postTable).where(eq(postTable.id, id));
+    const posts = db.select().from(postTable).where(eq(postTable.userId, id));
 
     return posts;
   },
+
+  // createPost: async (post: )
 };
